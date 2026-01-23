d76e3758f1a38946aa5e39c5d5c1aa5caeaf5117
21habdilah <158019954+21habdilah@users.noreply.github.com>
2026-01-23 19:21:58 +0700

    Update slides.js

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,15 +1,27 @@
-import SlideHero from "../components/slides/SlideHero.vue";
-import SlideRPL from "../components/slides/SlideRPL.vue";
-import SlideFlow from "../components/slides/SlideFlow.vue";
-import SlideTech from "../components/slides/SlideTech.vue";
-import SlideDeploy from "../components/slides/SlideDeploy.vue";
-import SlideClosing from "../components/slides/SlideClosing.vue";
+import SlideHero from '../components/slides/SlideHero.vue';
+import Slide01 from '../components/slides/Slide01_Pembuka.vue';
+import Slide02 from '../components/slides/Slide02_LatarBelakang.vue';
+import Slide03 from '../components/slides/Slide03_Definisi.vue';
+import Slide04 from '../components/slides/Slide04_Sejarah.vue';
+import Slide05 from '../components/slides/Slide05_Keunggulan.vue';
+import Slide06 from '../components/slides/Slide06_Struktur.vue';
+import Slide07 from '../components/slides/Slide07_CaraKerja.vue';
+import Slide08 from '../components/slides/Slide08_Visualisasi.vue';
+import Slide09 from '../components/slides/Slide09_Penegasan.vue';
+import Slide10 from '../components/slides/Slide10_Penutup.vue';
+import Slide11 from '../components/slides/Slide11_QnA.vue';
 
 export const slides = [
   { id: 1, component: SlideHero },
-  { id: 2, component: SlideRPL },
-  { id: 3, component: SlideTech },
-  { id: 4, component: SlideFlow }, 
-  { id: 5, component: SlideDeploy },
-  { id: 6, component: SlideClosing },
+  { id: 2, component: Slide01 },
+  { id: 3, component: Slide02 },
+  { id: 4, component: Slide03 },
+  { id: 5, component: Slide04 },
+  { id: 6, component: Slide05 },
+  { id: 7, component: Slide06 },
+  { id: 8, component: Slide07 },
+  { id: 9, component: Slide08 },
+  { id: 10, component: Slide09 },
+  { id: 11, component: Slide10 },
+  { id: 12, component: Slide11 }
 ];
